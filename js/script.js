1f94def06c2340cd22a78130fda4ca02edc7de10
YJS <x-height@naver.com>
2018-02-27 10:36:05 +0900

    no message

--- js/script.js
+++ js/script.js
@@ -124,3 +124,40 @@ $('ul.item li button').each(function(i) {
         $('div.none').eq(i).show();
     });
 });
+
+
+window.onload = function() {
+    startLoadFile();
+};
+
+function startLoadFile() {
+    $.ajax({
+        url : 'img.json',
+        type : 'GET',
+        dataType : 'json',
+        success : function(data) {
+            createImages(data)
+        }
+    });
+}
+
+// JSON 포멧 데이터 처리
+function createImages(data) {
+    var images = data.images;
+    console.log(images);
+
+    var strDOM = "";
+    for (var i = 0; i < images.length; i++) {
+        // N번째 이미지 정보를 구하기
+        var image = images[i];
+        // N번째 이미지 패널을 생성
+        // strDOM += '<div class="image_panel">';
+        strDOM += '<img src="' + image.url + '">';
+        strDOM += '<p class="title">' + image.title + '</p>';
+        // strDOM += '</div>';
+    }
+    // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+    var book = $("#book");
+    book.append(strDOM);
+    console.log(book);
+}
